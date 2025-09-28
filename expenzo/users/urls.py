a0e4d4e3c9cea6a4e89c7028af908e50fcc60117
RustemYeldessov <eldesov1997@gmail.com>
2025-09-28 17:32:25 +0500

    Add app "users"

---
+++ expenzo/users/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from expenzo.users import views
+
+
+urlpatterns = [
+    path("", views.UsersListView.as_view(), name="users_index"),
+    path("login/", views.UserLoginView.as_view(), name="login"),
+    path("logout/", views.UserLogoutView.as_view(), name="logout"),
+    path("create/", views.UsersCreateView.as_view(), name="users_create"),
+    path(
+        "<int:pk>/update/",
+        views.UserUpdateView.as_view(),
+        name="users_update"
+    ),
+    path(
+        "<int:pk>/delete/",
+        views.UserDeleteView.as_view(),
+        name="users_delete"
+    ),
+]
