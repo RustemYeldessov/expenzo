67ab25551f96cfca90833619c8cdc7083e32d24e
RustemYeldessov <eldesov1997@gmail.com>
2025-12-14 17:21:14 +0500

    feat: Add expenzo/urls,py

--- expenzo/urls.py
+++ expenzo/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='index')
 ]
