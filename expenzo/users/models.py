c1c4e8ffc92a814809fcedfec42deda8ca69bafe
RustemYeldessov <eldesov1997@gmail.com>
2025-12-15 20:31:58 +0500

    feat: Add users/models.py

---
+++ expenzo/users/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    first_name = models.CharField(
+        max_length=100,
+        blank=False,
+        verbose_name=_("First Name")
+    )
+    last_name = models.CharField(
+        max_length=100,
+        blank=False,
+        verbose_name=_("Last Name")
+    )
+    USERNAME_FIELD = 'username'
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
