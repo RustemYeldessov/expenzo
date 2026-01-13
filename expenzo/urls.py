e2da4d013266fbe5397fb630d738d84868a79b15
RustemYeldessov <eldesov1997@gmail.com>
2026-01-13 11:14:26 +0500

    feat: Add "users" to urls.py

--- expenzo/urls.py
+++ expenzo/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from .views import IndexView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index')
+    path("admin/", admin.site.urls),
+    path("", IndexView.as_view(), name='index'),
+
+    path("users", include(("expenzo.users.urls", "users"), namespace="users")),
 ]
