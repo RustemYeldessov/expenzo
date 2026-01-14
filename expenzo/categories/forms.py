de2a783fa00843a66a1f56eef602f50b004bf028
RustemYeldessov <eldesov1997@gmail.com>
2026-01-14 10:32:36 +0500

    fix: Change name from Categories to Category

--- expenzo/categories/forms.py
+++ expenzo/categories/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
-from .models import Categories
+from .models import Category
 
 
 class CategoryForms(forms.ModelForm):
     class Meta:
-        model = Categories
+        model = Category
         fields = ['name', 'description']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4}),
