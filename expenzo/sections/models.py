26ea38a73fd8e29429b015271dd1a9849b0306fa
RustemYeldessov <eldesov1997@gmail.com>
2026-01-14 10:23:11 +0500

    feat: Create sections/models.py

---
+++ expenzo/sections/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Sections(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
