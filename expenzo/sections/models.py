3511bc6f895d525ce7d9b941e4025ccb316f5081
RustemYeldessov <eldesov1997@gmail.com>
2026-01-14 10:31:59 +0500

    fix: Change name from Sections to Section

--- expenzo/sections/models.py
+++ expenzo/sections/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Sections(models.Model):
+class Section(models.Model):
     name = models.CharField(max_length=100, unique=True)
     created_at = models.DateField(auto_now=True)
 
