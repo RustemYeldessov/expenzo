fdc07fecfc11d953cd05e51c46791dfa50e957d3
RustemYeldessov <eldesov1997@gmail.com>
2026-01-14 12:45:02 +0500

    feat: Create sections/urls.py

---
+++ expenzo/sections/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from . import views
+
+app_name = "sections"
+
+urlpatterns = [
+    path("create/", views.SectionCreateView.as_view, name="create"),
+    path(
+        "<int:pk>/update/",
+        views.SectionUpdateView.as_view,
+        name="update"
+    ),
+    path(
+        "<int:pk>/delete/",
+        views.SectionDeleteView.as_view,
+        name="delete"
+    ),
+    path("", views.SectionListView.as_view, name="index")
+]
