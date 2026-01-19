e2cf1b3a895f07442f3a514419a1726591e2f04d
RustemYeldessov <eldesov1997@gmail.com>
2026-01-19 11:35:35 +0500

    fix: Change path name from "detail" to "show"

--- expenzo/expenses/urls.py
+++ expenzo/expenses/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "expenses"
 
 urlpatterns = [
-    path("<int:pk>/detail/", views.ExpenseDetailView.as_view(), name="detail"),
+    path("<int:pk>/detail/", views.ExpenseDetailView.as_view(), name="show"),
     path("create/", views.ExpenseCreateView.as_view(), name="create"),
     path("<int:pk>/update/", views.ExpenseUpdateView.as_view(), name="update"),
     path("<int:pk>/delete/", views.ExpenseDeleteView.as_view(), name="delete"),
