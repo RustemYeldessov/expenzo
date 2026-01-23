55c8bd75212ae28c7bed4cb18219a60b863b26f7
RustemYeldessov <eldesov1997@gmail.com>
2026-01-23 15:07:01 +0500

    refactor: Delete "unique=True" from name of section

--- expenzo/sections/models.py
+++ expenzo/sections/models.py
@@ -1,12 +1,20 @@
+from dataclasses import fields
+
 from django.db import models
 from django.contrib.auth.models import User
 
 
 class Section(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'name'], name='unique_user_section')
+        ]
+        verbose_name_plural = "Sections"
 
     def __str__(self):
         return self.name
