62674e8a2488f010b2378b37f6e58abf23dc415e
RustemYeldessov <eldesov1997@gmail.com>
2026-01-27 14:17:50 +0500

    feat: Create expenese/api_views.py

---
+++ expenzo/expenses/api_views.py
@@ -0,0 +1,23 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
+from .models import Expense
+from .serializers import ExpenseSerializer
+
+
+class ExpenseAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        expenses_list = Expense.objects.all().value()
+        return Response({'expenses': list(expenses_list)})
+
+    def post(self, request):
+        serializer = ExpenseSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
