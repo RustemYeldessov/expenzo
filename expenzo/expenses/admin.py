443e75186e30d637306050b60a0b2467630345b6
RustemYeldessov <eldesov1997@gmail.com>
2026-02-03 13:11:18 +0500

    refactor: Create custom User

--- expenzo/expenses/admin.py
+++ expenzo/expenses/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from expenzo.expenses.models import Expense
+from expenzo.categories.models import Category
+from expenzo.sections.models import Section
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'description', 'amount', 'category', 'user')
+    list_filter = ('date', 'category', 'section')
+    search_fields = ('description',)
+
+admin.site.register(Category)
+admin.site.register(Section)
